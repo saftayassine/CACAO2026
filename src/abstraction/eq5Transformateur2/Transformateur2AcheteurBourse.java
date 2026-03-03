96a08931538259ca273048973d9fe8d7614dc6a7
Maxence Lepetit <maxence.lepetit@imt-atlantique.net>
2026-03-03 09:52:48 +0100

    Transformateur2AcheteurBourse

---
+++ src/abstraction/eq5Transformateur2/Transformateur2AcheteurBourse.java
@@ -0,0 +1,41 @@
+package abstraction.eq5Transformateur2;
+
+import java.util.List;
+import abstraction.eqXRomu.produits.Gamme;
+import abstraction.eqXRomu.bourseCacao.IAcheteurBourse;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.produits.Feve;
+/**
+ * Auteur Maxence
+ */
+public class Transformateur2AcheteurBourse extends Transformateur2Stock implements IAcheteurBourse {
+
+
+    public Transformateur2AcheteurBourse(List<Integer> stock){
+        super();
+    }
+    public double demande(Feve f, double cours) {
+
+        if (f.getGamme()==Gamme.MQ){
+            return 80;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    
+    public void notificationAchat(Feve f, double quantiteEnT, double coursEnEuroParT) {
+        this.getJournaux().get(1).ajouter("Achat effectué de: "+quantiteEnT+" fèves "+f+" au prix/tonne de "+coursEnEuroParT);
+        this.getJournaux().get(4).ajouter("Achat effectué de: "+quantiteEnT+" fèves "+f+" au prix/tonne de "+coursEnEuroParT);
+
+        this.add_feve(quantiteEnT,f);
+    }
+
+    
+    public void notificationBlackList(int dureeEnStep) {
+        this.getJournaux().get(4).ajouter("Nous avons été blacklistés pour "+dureeEnStep+" étapes.");
+    }
+    
+
+}
