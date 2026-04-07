ef50e8bc6866e71851d88f539a5b53b2bb4d0b1e
Vassili Spiridonov <vassili.spiridonov@imt-atlantique.net>
2026-04-07 08:59:41 +0200

    Modif Agriculteur3

--- src/abstraction/eq3Producteur3/Agriculteurs3.java
+++ src/abstraction/eq3Producteur3/Agriculteurs3.java
@@ -2,37 +2,59 @@ package abstraction.eq3Producteur3;
 
 /** @author Vassili Spiridonov*/
 public class Agriculteurs3 {
-    private int nbEmployesAdulte;  
-    private int nbEmployesEnfant;    
-    private double salaireAdulte;    
+    private int nbCDI;  
+    private int nbInterim;
+    private int nbEnfant;    
+    private double salaireCDI; 
+    private double salaireInterim;     
     private double salaireEnfant;   
     private boolean exploitationEnfant; 
 
-    public void Agriculteur3(int nbAdulte, int nbEnfant) {
-        this.nbEmployesAdulte = nbAdulte;
-        this.nbEmployesEnfant = 0; // Entrerpise éthique : aucun enfants exploités 
-        this.salaireAdulte = 12.0; // On les rémunères au max décidé dans les règles de fonctionnement (0.8€/jour)
+    public void Agriculteur3(int nbCDIInitial) {
+        this.nbCDI = nbCDIInitial;
+        this.nbInterim = 0;
+        this.nbEnfant = 0; // Entrerpise éthique : aucun enfants exploités 
+        this.salaireCDI = 12.0; // On les rémunères au max décidé dans les règles de fonctionnement (0.8€/jour)
+        this.salaireInterim = 20.0; // On paye plus chère les intérimaires
         this.salaireEnfant = 3.0;  // D'après les règles de fonctionnemments : 0.2€/jour
         this.exploitationEnfant = false; // On vérifie le respect de la charte éthique 
     }
 
     public double getForceDeTravailTotale() {
-        return this.nbEmployesAdulte + (this.nbEmployesEnfant * 0.6); /* Les enfants comptent pour 0.6 d'un adulte.
+        return this.nbCDI + (this.nbEnfant * 0.6) + this.nbInterim; /* Les enfants comptent pour 0.6 d'un adulte.
      */
     }
 
     public double getCoutMainOeuvreTotal() {
-        return (this.nbEmployesAdulte * this.salaireAdulte) + 
-               (this.nbEmployesEnfant * this.salaireEnfant);
+        return (this.nbCDI * this.salaireCDI) + (this.nbCDI ) + (this.nbInterim * this.salaireInterim) + (this.nbInterim )
+               (this.nbEnfant * this.salaireEnfant);
     }
 
-    //Cette fonction décrit notre engagement éthique concernant l'exploitation d'enfant 
+    //On Embauche des CDI 
+    public void EmbaucheCDI(int nb) { 
+        this.nbCDI += Math.max(0, nb);
+    }
+
+    //On licencie des CDI
+    public void licencieCDI(int nb) {
+        this.nbCDI -= Math.max(0, nb);
+    }
+
+    //Recrutement ponctuel des intérimaires 
+    public void setNbInterim(int nb) {
+        this.nbInterim = Math.max(0, nb);
+    }
+
+
+
+    //Cette fonction décrit notre engagement éthique concernant l'exploitation d'enfants
     public String getEthiqueEnfant() {
-        if (!this.exploitationEnfant && this.nbEmployesEnfant == 0) {
+        if (!this.exploitationEnfant && this.nbEnfant == 0) {
             return "Pas d'enfants exploités";
         }
         return "Exploitation non conforme aux standards éthiques.";
     }
+
 
 
 }
