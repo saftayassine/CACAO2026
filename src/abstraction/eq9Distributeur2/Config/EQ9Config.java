2d25fd24ef0c8771c17b90d9738fa68794227f25
Paul Juhel <p25juhel@imt-atlantique.net>
2026-05-26 11:25:33 +0200

    config

--- src/abstraction/eq9Distributeur2/Config/EQ9Config.java
+++ src/abstraction/eq9Distributeur2/Config/EQ9Config.java
@@ -3,7 +3,7 @@ package abstraction.eq9Distributeur2.Config;
 /**
  * @author Paul JUHEL
  * Centralise les constantes
- * Unités : tonnes pour les quantités (suffixe _T), kg si précisé (suffixe _KG), €/T pour les prix.
+ * Unités : tonnes pour les quantités (suffixe _T), €/T pour les prix.
  */
 public class EQ9Config {
     private EQ9Config() {}
@@ -17,8 +17,6 @@ public class EQ9Config {
 
     // Coûts et frais (€/tonne)
     public static final double FRAIS_STOCKAGE_EUR_PAR_T = 120.0;
-    public static final double COUT_STOCKAGE_PAR_TONNE = 500.0;
-    public static final double COUT_PENURIE_PAR_TONNE = 2000.0;
 
     // Stratégie
     public static final double PART_MARCHE_CIBLE = 0.45;      // 45%
